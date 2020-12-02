633a22f14c1e34af26a7006db526950d982c511d
Kskulski <firestar345@gmail.com>
2020-12-02 17:29:59 -0600

    update

---
+++ TaskMaster.js
@@ -0,0 +1,80 @@
+//pass input for new list function?
+function newItem() {
+    var input = document.getElementById("newInput").value;
+    addItem(input);
+}
+
+function addItem(input) {
+    var item = document.createElement("li");
+    var text = document.createTextNode(input);
+    item.appendChild(text);
+    if (input == "") {
+        alert("Write something first!");
+    } else {
+        document.getElementById("itemUL").appendChild(item);
+    }
+    document.getElementById("newInput").value = "";
+    addX(item);
+}
+
+function addX(item) {
+    var close = document.createElement("span");
+    var text = document.createTextNode("X");
+    close.className = "X";
+    close.appendChild(text);
+    item.appendChild(close);
+    xUpdate();
+}
+
+function xUpdate() {
+    var X = document.getElementsByClassName("X");
+        var i;
+        for (i = 0; i < X.length; i++) {
+            X[i].onclick = function() {
+                var item = this.parentElement;
+                item.remove();
+            }
+        }
+}
+
+function importWindow() {
+    window.open("file:///C:/Users/Kevin/Documents/github/kskulski-TaskMaster/importWindow.html");
+}
+
+function chooseEx() {
+    var list = document.getElementById("exampleUL");
+    var i;
+    for (i = 0; i < list.length; i++) {
+        list[i].onclick = function() {
+            importList(list[i]);
+            window.close();
+        }
+    }
+}
+
+function importList(example) {
+    var list = JSON.parse("file:///C:/Users/Kevin/Documents/github/kskulski-TaskMaster/" + example.value + ".json")
+    var i;
+    for (i = 0; i < list.length; i++) {
+        addItem(list[i]);
+    }
+}
+
+function login() {
+
+}
+
+function save() {
+
+}
+
+//new button?
+function load() {
+
+}
+
+//delete?
+function clearList() {
+    
+}
+
